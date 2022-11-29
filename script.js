a0de9bfffb5e210e6fcf55fd989ea2a0b9cface4
najeeb39 <96305189+najeeb39@users.noreply.github.com>
2022-11-30 04:42:58 +0530

    complete project

---
+++ script.js
@@ -0,0 +1,9 @@
+function myFunction() {
+    if(document.getElementById('email').value==""){
+      alert("Please Enter Your Email Address.!"); 
+    }
+    else {
+        alert("Thank You for Signing Up for Our News Letter.!");
+      }
+  }
+  
